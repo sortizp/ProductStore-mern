304a13128d9da2a8f793f5316d16844d5791c00f
sortizp <sortizp@bu.edu>
2026-06-02 02:05:33 -0400

    -Remove unnecessary IDE configuration files. -Updated the CreatePage.jsx with Mantine - updating POST

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware to parse JSON bodies
 app.use(express.json());
 app.use('/api/products', productRoutes);
-``
+
 // Start the server
 app.listen(PORT, () => {
     connectDB()
